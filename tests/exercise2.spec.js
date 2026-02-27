4c93a3a474ff6d012c409cb0d483b04718817f86
Sowndarya09 <53424618+Sowndarya09@users.noreply.github.com>
2026-02-27 23:01:55 +0000

    Exercise 3: Used toBeVisible, got the text content and took out the number validated the same

--- tests/exercise2.spec.js
+++ tests/exercise2.spec.js
@@ -32,3 +32,31 @@ test('Should click a book and open detail page', async ({ page }) => {
     await expect(page).toHaveURL(/light-in-the-attic/i);
 
 });
+
+
+// TEST 3: Click on a book that costs under £20 and check price is visible on the detail page
+test('Should click a book that costs under 20 and check price is visible on the detail page', async ({ page }) => {
+
+    // Step 1: Go to the site
+    await page.goto('http://books.toscrape.com');
+
+    // Step 2: Find the book title that costs under 20 and click it
+    await page.getByTitle('Set Me Free').click();
+
+    //Get title and print it in logs
+    const title = await page.title();
+    console.log('Page title is:', title);
+
+    // Step 3: Check the URL changed - we're on a detail page, using partial url match with hyphens - url doesn't have spaces
+    await expect(page).toHaveURL(/set-me-free/i);
+
+    //Step 4: Verify that the detail page has the price visible
+    await expect(page.locator('.col-sm-6.product_main h1')).toBeVisible();
+
+    //Step 5: Validate the price is under 20
+    const price = await page.locator('.col-sm-6.product_main p').first().textContent();
+    console.log('The Price is: ', price)
+    const priceNumber = parseFloat(price.replace(/[^0-9.]/g,''));
+    await expect(priceNumber).toBeLessThan(20);
+
+});
